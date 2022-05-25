93922c210e8f5835845611ca1fff5f53f6cd0c41
krichalski <106273704+krichalski@users.noreply.github.com>
2022-05-25 15:52:45 -0300

    Add files via upload

---
+++ Leitura.java
@@ -0,0 +1,29 @@
+//Nome: Kainã de Freitas Richalski RA: 2346214
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Leitura{
+
+	public String entDados(String rotulo){
+
+		System.out.println(rotulo);
+
+		InputStreamReader teclado = new InputStreamReader(System.in);
+		BufferedReader buff = new BufferedReader(teclado);
+		
+		String ret ="";
+			
+		try{
+			ret = buff.readLine();
+		}
+		catch(IOException ioe){
+			System.out.println("\nERRO de sistema: RAM - TECLADO");
+		}
+
+		return ret;
+	}
+
+}
+
